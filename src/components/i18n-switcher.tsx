dfebecee42d093cfae8e2afb63b4cb5634ee09b6
Florencea <bearflorencea@gmail.com>
2024-02-16 01:15:14 +0800

    fix: suspence

--- src/components/i18n-switcher.tsx
+++ src/components/i18n-switcher.tsx
@@ -1,6 +1,5 @@
 import { GlobalOutlined } from "@ant-design/icons";
 import { Button, Dropdown, type MenuProps } from "antd";
-import { Suspense } from "react";
 import { useTranslation } from "react-i18next";
 
 export const I18nSwitcher = () => {
@@ -18,27 +17,25 @@ export const I18nSwitcher = () => {
   ];
 
   return (
-    <Suspense>
-      <Dropdown
-        menu={{
-          selectable: true,
-          selectedKeys: [i18n.language],
-          items,
-          onClick: ({ key }) => {
-            i18n.changeLanguage(key);
-          },
-        }}
-        trigger={["click"]}
-        arrow
+    <Dropdown
+      menu={{
+        selectable: true,
+        selectedKeys: [i18n.language],
+        items,
+        onClick: ({ key }) => {
+          i18n.changeLanguage(key);
+        },
+      }}
+      trigger={["click"]}
+      arrow
+    >
+      <Button
+        type="text"
+        className="flex items-center justify-center"
+        title={t("changelocale")}
       >
-        <Button
-          type="text"
-          className="flex items-center justify-center"
-          title={t("changelocale")}
-        >
-          <GlobalOutlined />
-        </Button>
-      </Dropdown>
-    </Suspense>
+        <GlobalOutlined />
+      </Button>
+    </Dropdown>
   );
 };
